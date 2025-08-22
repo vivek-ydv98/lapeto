ea901976be9382501f115fb2098f6e2199b0c491
vivek yadav <vivekyadav8101998@gmail.com>
2025-08-22 22:05:35 +0530

    init commit

--- app/src/main/java/com/vivek/lapeto/data/models/FoodCategoryData.kt
+++ app/src/main/java/com/vivek/lapeto/data/models/FoodCategoryData.kt
@@ -4,6 +4,6 @@ package com.vivek.lapeto.data.models
 
 data class FoodCategoryData(val name: String, val img: Int)
 
-data class TabItem(val title: String)
+data class TabItem(val title: String )
 
 data class FoodCategory(val name: String, val imgRes: Int)
